c9a90b82657546578b1daa4347bd4e8f700cb63f
YassineGharabi <gharabiyassine18@gmail.com>
2025-03-06 14:42:39 +0100

    Add Authentification

---
+++ ormva-backend-project/app/Http/Controllers/AuthController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use App\Http\Requests\UserRequest;
+use Illuminate\Support\Facades\Hash;
+
+class AuthController extends Controller
+{
+    
+    public function register(UserRequest $request){
+
+
+        $fields = $request->validated();
+        
+        $user = User::create($fields);
+
+        $token = $user->createToken($request->name);
+
+        return [
+            'user' => $user,
+            'token' => $token->plainTextToken
+        ];
+    }
+
+    public function login(Request $request){
+        
+        $request->validate([
+            'email' => 'required|email|exists:users',
+            'password' => 'required|min:8'
+        ]);
+
+        $user = User::where( 'email' , $request->email )->first();
+
+        // if user with this email not exist in db or password not match return error
+        if(!$user || !Hash::check( $request->password , $user->password )){
+            return [
+                'errors' => [
+                    'email'=> ['The provided credentials are incorrect.']
+                ]
+            ];
+        }
+
+        $token = $user->createToken($user->name);
+
+        return [
+            'user' => $user ,
+            'token' => $token->plainTextToken
+        ];
+
+    }
+
+    public function logout(Request $request){
+        // delete all tokens that belong to this user 
+        $request->user()->tokens()->delete();
+
+        return [
+            'message' => 'You are logged out.'
+        ];
+
+    }
+
+
+}
