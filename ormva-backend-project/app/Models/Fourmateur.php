0f8e39ba1033230fadd726c3a05d999192a22d26
YassineGharabi <gharabiyassine18@gmail.com>
2025-03-28 15:23:32 +0100

    one change

--- ormva-backend-project/app/Models/Fourmateur.php
+++ ormva-backend-project/app/Models/Fourmateur.php
@@ -4,9 +4,11 @@ namespace App\Models;
 
 use App\Models\Formation;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Fourmateur extends Model
 {
+
     protected $fillable = [
         'nom',
         'contact',
